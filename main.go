4da3cc71f283ad19a34c4dc4faef8254012dc69f
Daro <daropl12@gmail.com>
2021-11-30 23:16:59 -0300

    file handling and request to recicla ia

--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const (
-	sendMessage = "https://api.telegram.org/bot%s/sendMessage"
-	getFile     = "https://api.telegram.org/bot%s/getFile?file_id=%s"
-	file        = "https://api.telegram.org/file/bot%s/%s"
+	sendMessageEndpoint = "https://api.telegram.org/bot%s/sendMessage"
+	getFileEndpoint     = "https://api.telegram.org/bot%s/getFile?file_id=%s"
+	fileEndpoint        = "https://api.telegram.org/file/bot%s/%s"
 )
 
 type webhookReqBody struct {
@@ -41,30 +44,55 @@ type PhotoSize struct {
 	FileSize     int    `json:"file_size"`
 }
 
+type FileResponse struct {
+	OK     bool `json:"ok"`
+	Result File `json:"result"`
+}
+
 type File struct {
 	FileID   string `json:"file_id"`
 	FilePath string `json:"file_path"`
 }
 
+type AWSResponse struct {
+	Class       string `json:"class"`
+	Probability string `json:"probability"`
+}
+
 // This handler is called everytime telegram sends us a webhook event
 func Handler(res http.ResponseWriter, req *http.Request) {
-	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	telegramToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 
 	// First, decode the JSON response body
 	reqBody := &webhookReqBody{}
 	if err := json.NewDecoder(req.Body).Decode(reqBody); err != nil {
 		fmt.Printf("could not decode request body: %s\n", err)
+		res.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	fmt.Printf("reqBody: %+v", reqBody)
 
-	if err := makeRequest(reqBody.Message, token); err != nil {
-		fmt.Printf("could send response: %s\n", err)
+	image, err := getFile(reqBody.Message.Photo[0].FileID, telegramToken)
+	if err != nil {
+		fmt.Printf("could not get image: %s\n", err)
+		res.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	// log a confirmation message if the message is sent successfully
-	fmt.Println("reply sent")
+	awsRes, err := classify(image)
+	if err != nil {
+		fmt.Printf("could not classify image: %s\n", err)
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := makeRequest(reqBody.Message.Chat.ID, telegramToken, awsRes); err != nil {
+		fmt.Printf("could send response: %s\n", err)
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res.WriteHeader(http.StatusOK)
 }
 
 // Server listen to port 3000
@@ -74,40 +102,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	} */
+
 	port := os.Getenv("PORT")
 
 	fmt.Printf("Listen to port %s\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), http.HandlerFunc(Handler))
 }
 
-func makeRequest(message Message, telegramToken string) error {
+func makeRequest(chatID int64, telegramToken string, awsResponse AWSResponse) error {
 	resBody := sendMessageReqBody{
-		ChatID: message.Chat.ID,
-		Text:   "HI!",
+		ChatID: chatID,
+		Text:   fmt.Sprintf("Hay un %s de probabilidad que su residuo sea de tipo %s", awsResponse.Probability, awsResponse.Class),
 	}
 
 	// Create the JSON body from the struct
 	resBytes, err := json.Marshal(resBody)
 	if err != nil {
+		fmt.Printf("could not marshal response: %s\n", err)
 		return err
 	}
-
-	getFileRes, err := http.Get(fmt.Sprintf(getFile, telegramToken, message.Photo[0].FileID))
-	if err != nil {
-		fmt.Printf("getfile response error: %s\n", err)
-		return err
-	}
-	defer getFileRes.Body.Close()
-	file := &File{}
-	if err := json.NewDecoder(getFileRes.Body).Decode(file); err != nil {
-		fmt.Printf("could not decode file: %s\n", err)
-		return err
-	}
-	fmt.Printf("File: %+v\n", file)
 
 	// Send a post request with your token
-	sendMessageRes, err := http.Post(fmt.Sprintf(sendMessage, telegramToken), "application/json", bytes.NewBuffer(resBytes))
+	sendMessageRes, err := http.Post(fmt.Sprintf(sendMessageEndpoint, telegramToken), "application/json", bytes.NewBuffer(resBytes))
 	if err != nil {
+		fmt.Printf("send message response error: %s\n", err)
 		return err
 	}
 	defer sendMessageRes.Body.Close()
@@ -117,4 +135,68 @@ func makeRequest(message Message, telegramToken string) error {
 	}
 
 	return nil
+}
+
+func getFile(fileID, telegramToken string) ([]byte, error) {
+	getFileRes, err := http.Get(fmt.Sprintf(getFileEndpoint, telegramToken, fileID))
+	if err != nil {
+		fmt.Printf("getfile response error: %s\n", err)
+		return []byte{}, err
+	}
+	defer getFileRes.Body.Close()
+
+	fileResponse := &FileResponse{}
+	if err := json.NewDecoder(getFileRes.Body).Decode(fileResponse); err != nil {
+		fmt.Printf("could not decode file: %s\n", err)
+		return []byte{}, err
+	}
+
+	fileRes, err := http.Get(fmt.Sprintf(fileEndpoint, telegramToken, fileResponse.Result.FilePath))
+	if err != nil {
+		fmt.Printf("file response error: %s\n", err)
+		return []byte{}, err
+	}
+	defer fileRes.Body.Close()
+
+	// We read all the bytes of the image
+	// Types: data []byte
+	data, err := ioutil.ReadAll(fileRes.Body)
+	if err != nil {
+		fmt.Printf("read file error: %s\n", err)
+		return []byte{}, err
+	}
+
+	return data, nil
+}
+
+func classify(imageByte []byte) (AWSResponse, error) {
+	awsEndpoint := os.Getenv("RECICLA_IA_ENDPOINT")
+	req, err := http.NewRequest("POST", awsEndpoint, bytes.NewReader(imageByte))
+	if err != nil {
+		fmt.Printf("could not build request: %s\n", err)
+		return AWSResponse{}, err
+	}
+	req.Header.Set("Content-Type", "image/jpeg")
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	rsp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("classify response error: %s\n", err)
+		return AWSResponse{}, err
+	}
+	if rsp.StatusCode != http.StatusOK {
+		log.Printf("classify request failed with response code: %d", rsp.StatusCode)
+	}
+
+	awsResponse := &AWSResponse{}
+	if err := json.NewDecoder(rsp.Body).Decode(awsResponse); err != nil {
+		fmt.Printf("could not decode file: %s\n", err)
+		return AWSResponse{}, err
+	}
+
+	fmt.Printf("awsResponse: %v", awsResponse)
+	return *awsResponse, nil
 }
